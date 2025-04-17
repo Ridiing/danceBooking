ec46c7ae108d788a3bfd972eb26baa0604840f7e
“Ridiing” <riding_lewis@yahoo.co.uk>
2025-04-17 04:29:34 +0100

    Fix user registration to use correct userDB

--- models/userModel.js
+++
@@ -1,9 +0,0 @@
-const Datastore = require('nedb');
-const path = require('path');
-
-const db = new Datastore({
-  filename: path.join(__dirname, '../data/users.db'),
-  autoload: true
-});
-
-module.exports = db;
