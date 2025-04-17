81e59f73ee6f8720341ede5db495e07e78566a01
“Ridiing” <riding_lewis@yahoo.co.uk>
2025-04-17 07:44:38 +0100

    Fix route crash: corrected controller function name

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -2,13 +2,10 @@ const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingController');
 
-// Enroll in a course
-router.post('/enrol/:id', bookingController.enrol);
-
 // View user's own courses
 router.get('/my-courses', bookingController.showMyCourses);
 
 // Unenroll from a course (for logged-in user)
-router.post('/my-courses/unenrol/:courseId', bookingController.unenrol);
+router.post('/my-courses/unenrol/:courseId', bookingController.unenrolFromMyClass);
 
 module.exports = router;
